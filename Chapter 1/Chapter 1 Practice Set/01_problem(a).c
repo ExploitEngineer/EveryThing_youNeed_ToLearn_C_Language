d2e6568aa26503185b9149ccc0e6b94394866dd2
DSnake0 <revilwhoami0098@gmail.com>
2025-01-12 14:16:55 +0500

    Add Practice Set of Chapter 1 in Chapter 1

---
+++ Chapter 1/Chapter 1 Practice Set/01_problem(a).c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int lenght = 3;
+    int breadth = 6;
+    printf("The area of this rectangle is %d", lenght * breadth);
+    return 0;
+}
---
+++ Chapter 1/Chapter 1 Practice Set/01_problem(b).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int lenght, breadth;
+    printf("Enter lenght\n");
+    scanf("%d", &lenght);
+    printf("Enter breadth\n");
+    scanf("%d", &breadth);
+    printf("The area of reactangle is %d", lenght*breadth);
+    return 0;
+}

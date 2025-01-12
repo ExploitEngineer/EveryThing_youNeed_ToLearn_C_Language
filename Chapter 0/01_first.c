d957c11572c14f64bafc505279f125a842f5fd13
DSnake0 <revilwhoami0098@gmail.com>
2025-01-12 12:40:47 +0500

    Add c language NOtes & Cheatsheet, Add C compiler, Create C History, Create Chapter 0

---
+++ Chapter 0/01_first.c
@@ -0,0 +1,9 @@
+// HACK: This line is a preprocessor directive that tells the compiler to include the standard input-output header file `stdio.h`. This header file contains declarations for input and output functions such as `printf`
+#include <stdio.h>
+
+// HACK: This is the main function which is the entry point of the program
+int main()
+{
+    printf("Hello World"); // Prints hello world
+    return 0;              // return 0 (means function end)
+}

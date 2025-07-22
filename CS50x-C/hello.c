f086fa090343cbcb136ebf2a2e07104d41c316ae
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-22 06:29:59 +0500

    feat: add cs50 program
    - add hello, <your-name> program
    - add cs50 libraray
    - compile the program

--- CS50x-C/hello.c
+++ CS50x-C/hello.c
@@ -1,3 +1,7 @@
 #include <stdio.h> // any file that ends with .h is header file
+#include <cs50.h>
 
-int main(void) { printf("hello, world\n"); }
+int main(void) { 
+  string answer = get_string("What is your name ? ");
+  printf("hello, %s\n", answer);
+}

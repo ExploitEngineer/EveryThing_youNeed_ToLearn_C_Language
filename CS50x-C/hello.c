fbad5dd722355abd1d2170a5b9e06d9d1fd5023f
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-22 06:58:13 +0500

    refactor: hello.c formatting

--- CS50x-C/hello.c
+++ CS50x-C/hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h> // any file that ends with .h is header file
 #include <cs50.h>
 
-int main(void) { 
-  string answer = get_string("What is your name ? ");
-  printf("hello, %s\n", answer);
+int main(void)
+{
+    string answer = get_string("What is your name ? ");
+    printf("hello, %s\n", answer);
 }

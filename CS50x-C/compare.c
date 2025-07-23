40ee457b0fb7b3f61eb4f895a50b31abac082f6f
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-23 21:07:27 +0500

    refactor: compare.c
    added the full compare.c program

--- CS50x-C/compare.c
+++ CS50x-C/compare.c
@@ -7,5 +7,9 @@ int main(void) {
 
   if (x < y) {
     printf("x is less than y\n");
+  } else if (x > y) {
+    printf("x is not less than y\n");
+  } else {
+    printf("x is equal to y\n");
   }
 }

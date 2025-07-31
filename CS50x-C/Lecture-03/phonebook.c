d46cface101b2a37d509c50176db573404de805b
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-01 01:34:02 +0500

    refactor: add structs

--- CS50x-C/Lecture-03/phonebook.c
+++ CS50x-C/Lecture-03/phonebook.c
@@ -2,14 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+  string name;
+  string number;
+} person;
+
 int main(void) {
-  string names[] = {"Yuliia", "David", "John"};
-  string numbers[] = {"+1-617-495-1000", "+1-949-468-2750", "+1-617-495-1000"};
+  person people[3];
+
+  people[0].name = "David";
+  people[0].number = "+1-617-495-1000";
+
+  people[1].name = "John";
+  people[1].number = "+1-949-468-2750";
+
+  people[2].name = "Yuliia";
+  people[2].number = "+1-617-495-1000";
 
   string name = get_string("Name: ");
   for (int i = 0; i < 3; i++) {
-    if (strcmp(names[i], name) == 0) {
-      printf("Found %s\n", numbers[i]);
+    if (strcmp(people[i].name, name) == 0) {
+      printf("Found %s\n", people[i].number);
       return 0;
     }
   }

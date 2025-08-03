834a37537ada2889f11ef6f26a1a48a48fc02811
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-03 05:44:19 +0500

    refactor(format): agree.c

--- CS50x-C/agree.c
+++ CS50x-C/agree.c
@@ -1,11 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int mian(void) {
-  char c = get_char("Do you agree ?");
-  if (c == 'y' || c == 'Y') {
-    printf("Agreed.\n");
-  } else if (c == 'n' || c == 'N') {
-    printf("Not agreed.\n");
-  }
+int main (void)
+{
+    char c = get_char ("Do you agree ?");
+    if (c == 'y' || c == 'Y')
+    {
+        printf ("Agreed.\n");
+    }
+    else if (c == 'n' || c == 'N')
+    {
+        printf ("Not agreed.\n");
+    }
 }

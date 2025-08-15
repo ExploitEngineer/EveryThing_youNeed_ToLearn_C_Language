ce4ca1bdff66eb9d09f0714d3dba7904832d0ccf
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-15 14:35:43 +0500

    refactor: linked list

---
+++ CS50x-C/Lecture-05/linked-list.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cs50.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+} node;
+
+int main(void)
+{
+    node *list = NULL;
+
+    for(int i = 0; i < 3; i++)
+    {
+        node *n = malloc(sizeof(node));
+        if(n == NULL)
+        {
+            // TODO: free any memory already malloc'd
+            free(n);
+            return 1;
+        }
+
+        (*n).number = get_int("Number: ");
+        n->next = NULL;
+
+        // if list is empty
+        if(list == NULL)
+        {
+            list = n;
+        }
+        // if list has numbers already
+        else
+        {
+            for(node *ptr = list; ptr != NULL; ptr = ptr->next)
+            {
+                // if at end of list
+                if(ptr->next == NULL)
+                {
+                    ptr->next = n;
+                    break;
+                }
+            }
+        }
+    }
+
+    // Time passes
+    for(node *ptr = list; ptr != NULL; ptr = ptr->next)
+    {
+        printf("%i\n", ptr->number);
+    }
+
+    // Time passes
+    node *ptr = list;
+    while(ptr != NULL)
+    {
+        node *next = ptr->next;
+        free(ptr);
+        ptr = next;
+    }
+    return 0;
+}

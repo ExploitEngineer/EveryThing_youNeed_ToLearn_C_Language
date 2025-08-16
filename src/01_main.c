2a827ebc50f04a46575922071710f8c4cd91b9d8
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-16 07:10:16 +0500

    refactor: main.c

--- src/01_main.c
+++ src/01_main.c
@@ -5,13 +5,13 @@ int main()
 
     // This is my first program! :)
 
-        /*
-        This
-        is
-        also
-        a
-        comment
-        */
+    /*
+    This
+    is
+    also
+    a
+    comment
+    */
 
     printf("I like pizza!\n");
     printf("It's really good");

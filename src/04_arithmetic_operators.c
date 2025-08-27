433d9583f2851209589fcba9e86bfac874d1453b
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-27 07:44:35 +0500

    feat: add arithmetic operators

---
+++ src/04_arithmetic_operators.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // arithmetic operators = + - * / % ++ --
+
+    int x = 2;
+    int y = 3;
+    int z = 0;
+
+    // z = x + y;
+    // z = x - y;
+    // z = x * y;
+    // z = x / y;
+    // z = x % y;
+    // z = x % y;
+
+    // x++;
+    // x--;
+
+    x += 5;
+    x -= 5;
+    x *= 5;
+    x /= 5;
+
+    printf("%d", x);
+
+    return 0;
+}

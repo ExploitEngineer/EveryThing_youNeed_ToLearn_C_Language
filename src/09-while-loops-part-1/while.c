76e6b0c8b9d9d5652e3d99c274b5f9992edcf2b5
ExploitEngineer <revilwhoami0098@gmail.com>
2025-10-21 06:36:34 +0500

    feat: add whlie loops in c part-1

---
+++ src/09-while-loops-part-1/while.c
@@ -0,0 +1,19 @@
+/* while.c */
+
+#include <stdio.h>
+
+int main()
+{
+    int x;
+
+    x = 0;
+    while(x != 1)
+    {
+
+        printf("Do you wish to quit? Press 1.\n");
+        scanf("%d", &x);
+    }
+    printf("Important!\n");
+
+    return 0;
+}

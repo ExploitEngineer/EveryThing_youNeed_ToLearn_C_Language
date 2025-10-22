66684ced0ff75656ca98d1ae4b93c12315550df5
ExploitEngineer <revilwhoami0098@gmail.com>
2025-10-22 06:39:35 +0500

    feat: add if statements in c part-2

---
+++ src/12-if-statements-part-2/if.c
@@ -0,0 +1,26 @@
+/* if.c */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int speed;
+
+    printf("How fast are you driving? (km/h)\n");
+    scanf("%d", &speed);
+
+    if(speed > 100)
+    {
+        printf("You are driving to fast!\n");
+    }
+    else if(speed > 80)
+    {
+        printf("You are driving perfectly fine\n");
+    }
+    else
+    {
+        printf("OK\n");
+    }
+
+    return 0;
+}

b8de3fab7457050f9552310a32bf9c12d8190226
ExploitEngineer <revilwhoami0098@gmail.com>
2025-10-27 15:51:42 +0500

    feat: add malloc - dynamic memory allocation in c

---
+++ src/23-malloc/dynamic.c
@@ -0,0 +1,19 @@
+/* dynamic.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char *name;
+
+    name = malloc(32);
+
+    printf("What is your name?\n");
+    scanf("%s", name);
+    printf("Hello %s\n", name);
+
+    free(name);
+
+    return 0;
+}

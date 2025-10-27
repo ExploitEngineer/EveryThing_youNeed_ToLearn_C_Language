7bd21258d70ede050f5154d5c56c05bb80dfeec5
ExploitEngineer <revilwhoami0098@gmail.com>
2025-10-27 15:38:27 +0500

    feat: add references OR ampersand in c

---
+++ src/22-references/ampersand.c
@@ -0,0 +1,23 @@
+/* ampersand.c */
+
+#include <stdio.h>
+
+void addition(int a, int b, int *target)
+{
+    *target = a + b;
+
+    return;
+}
+
+int main(void)
+{
+    int x, y, answer;
+
+    x = 5;
+    y = 15;
+
+    addition(x, y, &answer);
+    printf("%d\n", answer);
+
+    return 0;
+}

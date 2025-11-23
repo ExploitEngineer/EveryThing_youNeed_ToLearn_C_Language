09e97fb159daa34cf667addc0856566fb007fdb1
ExploitEngineer <revilwhoami0098@gmail.com>
2025-11-23 14:23:47 +0500

    refactor: comments in c

--- c-by-exmaple/04-comments/main.c
+++ c-by-exmaple/04-comments/main.c
@@ -1,23 +1,12 @@
-#include <stdio.h>
-
 int main(void)
 {
-    unsigned int x = 2;
-    switch(x)
-    {
-    case 1:
-        printf("One");
-        break; // You must break the search or it will fall through to the next match.
-    case 2:
-        printf("Two");
-        break;
-    default: // If no match is found.
-        break;
-    }
-
     /*
-    Multi-line comments are written like this.
-    */
+     * Multi-line comments are written like this.
+     */
+
+    // printf("hello world");
+
+    // Single-line comments are written like this.
 
     return 0;
 }

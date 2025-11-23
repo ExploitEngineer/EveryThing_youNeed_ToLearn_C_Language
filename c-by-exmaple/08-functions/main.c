4ad9ce293b6971e163abfce5e6d31eb499a433ec
ExploitEngineer <revilwhoami0098@gmail.com>
2025-11-23 15:37:53 +0500

    feat: add functions in c

---
+++ c-by-exmaple/08-functions/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Double the number passed in as 'x', returning the new value to the function caller.
+int double_number_a(int x)
+{
+    return 2 * x;
+}
+
+// Double the number pointed to by 'x', storing the result in the original variable.
+void double_number_b(int *x)
+{
+    // *x = *x * 2; both of these lines are the same
+    *x *= 2;
+}
+
+int main(void)
+{
+    int num = 5;
+    printf("%d\n", double_number_a(num));
+    printf("%d\n", num);
+    double_number_b(&num);
+    printf("%d\n", num);
+    return 0;
+}

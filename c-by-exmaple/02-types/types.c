853b38830ca93ea362c66f03b6b3b4ac0c17c9c0
ExploitEngineer <revilwhoami0098@gmail.com>
2025-11-23 13:58:32 +0500

    feat: add data types in c

---
+++ c-by-exmaple/02-types/types.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+
+int main(void)
+{
+    int a;          // Integer
+    unsigned int b; // Unsinged integers only store positive numbers. As result, they have higher positive range.
+    char c;         // Character
+    short d;        // Short Integer
+    long e;         // Long Integer
+    float f;        // Floating point integer
+    double g;       // Double-precision floating point integer
+    bool h;         // Boolean TRUE or FLASE
+    return 0;
+}

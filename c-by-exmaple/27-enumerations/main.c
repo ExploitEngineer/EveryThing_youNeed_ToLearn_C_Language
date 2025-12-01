fa7b7d93fb8c4b5d32c052da2eb9bd580faac2ad
ExploitEngineer <revilwhoami0098@gmail.com>
2025-12-02 02:37:55 +0500

    feat: add enumerations in c

---
+++ c-by-exmaple/27-enumerations/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+enum week
+{
+    mon,
+    tue,
+    wed,
+    thu,
+    fri,
+    sat,
+    sun
+};
+
+int main(void)
+{
+    for(int i = mon; i <= sun; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}

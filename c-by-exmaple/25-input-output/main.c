5bb17cd4732d2dba7398e476033bbfd76a5c6e9e
ExploitEngineer <revilwhoami0098@gmail.com>
2025-12-02 02:33:28 +0500

    feat: add input & output in c

---
+++ c-by-exmaple/25-input-output/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    printf("Enter an integer value for x: ");
+    scanf("%d", &x);
+    printf("The value at x is now: %d\n", x);
+
+    return 0;
+}

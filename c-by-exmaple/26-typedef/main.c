c9113569a79610bf6ec2853456ec364b4320b4ce
ExploitEngineer <revilwhoami0098@gmail.com>
2025-12-02 02:35:54 +0500

    feat: add typedef in c

---
+++ c-by-exmaple/26-typedef/main.c
@@ -0,0 +1,9 @@
+// typedef is Used to add an additional name to a type. Useful when working with more complex types, making them more human readable.
+
+typedef unsigned char byte;
+
+int main(void)
+{
+    byte b1 = 'c';
+    return 0;
+}
